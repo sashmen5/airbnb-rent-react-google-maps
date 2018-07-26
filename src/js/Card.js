e2ede9bb2a5b5d84850eabfe5567f6ef64e18ed2
sashmen5 <30698617+sashmen5@users.noreply.github.com>
2018-07-26 21:43:39 +0300

    Picture loading refatored

--- src/js/Card.js
+++ src/js/Card.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropType from 'prop-types';
 import {priceFormat} from './utils/Formatters';
+import picture1 from '../assets/1.jpg';
+import picture2 from '../assets/2.jpg';
+import picture3 from '../assets/3.jpg';
+import picture4 from '../assets/4.jpg';
+import picture5 from '../assets/5.jpg';
 
 const Card = ({property, activeProperty, setActiveProperty}) => {
     const {price, address, picture, city, bedrooms, bathrooms, carSpaces, index} = property;
     const activeClass = property === activeProperty ? 'is-active': '';
+    const pictures = {
+        'picture1': picture1,
+        'picture2': picture2,
+        'picture3': picture3,
+        'picture4': picture4,
+        'picture5': picture5,
+    };
     return <div
         id={`card-${index}`}
         onClick={()=>{setActiveProperty(property, false)}}
         className={`card col-sm-12 col-md-6 col-lg-4 ${activeClass}`}>
-        <img src={picture} alt="Singer" />
+        <img src={pictures[picture]} alt="Singer" />
         <p className="price">{priceFormat(price)}</p>
         <div className="details">
             <span className="index">{index + 1}</span>
